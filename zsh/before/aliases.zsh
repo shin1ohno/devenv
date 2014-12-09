543f75625eb8e836c7c2e37137927ad16000c23c
Shinichi Ohno <shin1ohno@me.com>
2014-12-09 23:49:14 +0900

    Zsh lists files after change current directory

--- zsh/before/aliases.zsh
+++ zsh/before/aliases.zsh
@@ -1,2 +1,4 @@
 hash -d work=~/Documents/work
 alias sc="source ~/.zshrc"
+function list_files_of_current_dir() { script -q /dev/null ls -GFa . | tr -d '\r' | cat }
+function chpwd() { list_files_of_current_dir }
