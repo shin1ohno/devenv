bdf0f7d2f0ceac30983becb871970f21cc21e93b
Shin'ichi Ohno <ohno@cookpad.com>
2020-04-09 22:58:47 +0900

    Add fzf change directory and fzf nvim

--- zsh/before/aliases.zsh
+++ zsh/before/aliases.zsh
@@ -2,3 +2,26 @@ hash -d work=~/Documents/work
 alias sc="source ~/.zshrc"
 function list_files_of_current_dir() { script -q /dev/null ls -GFa . | tr -d '\r' | cat }
 function chpwd() { list_files_of_current_dir }
+
+#fzf
+unalias ff
+ff() {
+  local dir
+  dir=$(find ${1:-.} -maxdepth 5 -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+                  cd "$dir"
+}
+fzf-cd() {
+  local dir
+  dir=$(find ${1:-.} -maxdepth 5 -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+                  BUFFER="cd $dir"
+  zle accept-line
+}
+zle -N fzf-cd
+bindkey '^f' fzf-cd
+
+alias fv='fzf-nvim'
+fzf-nvim() {
+  nvim $(fzf)
+}
